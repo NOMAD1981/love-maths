eb8c1525745e044199bd4d7802d002b109bd6fac
NOMAD1981 <niamh@nomad.ie>
2022-02-27 15:21:46 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// wait for the dom to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+// add listener to wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    // more modern, cleaner version of for (let i = 0; i < buttons.length; i++)
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                // note template literal used | backquotes, not apostrophes or backquotes
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
 
 }
@@ -14,6 +35,10 @@ function incrementScore() {
     
 }
 
+function incrementWrongAnswer() {
+    
+}
+
 function displayAdditionQuestion() {
     
 }
@@ -26,6 +51,6 @@ function displayMultiplyQuestion() {
     
 }
 
-function displayAdditionQuestion() {
+// function displayDivideQuestion() {
     
-}
+// }
